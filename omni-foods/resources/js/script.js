36404b1e7dc09628b0bea86cc71120d3d859c071
Sean Grant <seangrant118@gmail.com>
2021-04-06 13:54:49 -0400

    scroll nav

--- omni-foods/resources/js/script.js
+++ omni-foods/resources/js/script.js
@@ -28,4 +28,30 @@ $(document).ready(function () {
       1000
     );
   });
+
+  // nav scroll
+
+  $(function () {
+    $("a[href*=#]:not([href=#])").click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+  });
 });
